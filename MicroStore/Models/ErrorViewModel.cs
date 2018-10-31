f343b79d0d9a7187fa9b7bd94dd3dc93a62ee03b
Görkem Öztürk <gorkemozturk@hotmail.com>
2018-10-31 17:58:36 +0300

    Proje dosyası ekle.

---
+++ MicroStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MicroStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
